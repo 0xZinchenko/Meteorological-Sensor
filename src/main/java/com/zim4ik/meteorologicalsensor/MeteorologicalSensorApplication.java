c093c49705e76e723b2ed5b9fd02167cc22778a6
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2026-05-20 17:50:35 +0200

    initial commit

---
+++ src/main/java/com/zim4ik/meteorologicalsensor/MeteorologicalSensorApplication.java
@@ -0,0 +1,13 @@
+package com.zim4ik.meteorologicalsensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorologicalSensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteorologicalSensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zim4ik/meteorologicalsensor/MeteorologicalSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zim4ik.meteorologicalsensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorologicalSensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
